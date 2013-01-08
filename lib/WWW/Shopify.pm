ba9ee424924ef9157807bf12c1524948bcdce5d5
Adam Harrison <adamdh@cpan.org>
2013-01-08 22:39:37 +0000

    Import of ADAMDH/WWW-Shopify-0.04 from CPAN.
    gitpan-cpan-distribution: WWW-Shopify
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         ADAMDH/WWW-Shopify-0.04.tar.gz
    gitpan-cpan-author:       ADAMDH
    gitpan-cpan-maturity:     released

--- lib/WWW/Shopify.pm
+++ lib/WWW/Shopify.pm
@@ -66,7 +66,7 @@ use URI::Escape;
 
 package WWW::Shopify;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 use constant {
 	RELATION_OWN_ONE => 0,
@@ -356,7 +356,13 @@ Adam Harrison (adamdharrison@gmail.com)
 
 =head1 LICENSE
 
-MIT License
+Copyright (C) 2013 Adam Harrison
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 =cut
 
